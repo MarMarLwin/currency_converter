a5741fb43f474779e277d67c5f62ca8087d51e3d
Mar Mar Lwin <lwin.ppo369@gmail.com>
2022-03-16 13:56:18 +0630

    add comment

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -5,7 +5,12 @@ import 'package:currency/util/app_constants.dart';
 import 'package:get/get.dart';
 
 Future<void> init() async {
+  //api client
   Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.appBaseUrl));
+
+  //repos
   Get.lazyPut(() => CurrencyRepo(apiClient: Get.find()));
+
+  //controllers
   Get.lazyPut(() => CurrencyController(currencyRepo: Get.find()));
 }
